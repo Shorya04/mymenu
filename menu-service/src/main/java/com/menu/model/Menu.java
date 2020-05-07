d008a3257bb43b7e5ab1bb3b43c4800ff0e9751f
Shorya Singh <shorya.4.singh@gmail.com>
2020-05-07 15:23:55 +0530

    commit

--- menu-service/src/main/java/com/menu/model/Menu.java
+++ menu-service/src/main/java/com/menu/model/Menu.java
@@ -1,8 +1,7 @@
 package com.menu.model;
 
-import java.util.ArrayList;
-import java.util.List;
 
+import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- menu-service/src/main/java/com/menu/model/Price.java
+++ menu-service/src/main/java/com/menu/model/Price.java
@@ -5,11 +5,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 @Entity
 public class Price  {
