575a1ec811ff52f4e3420e629e2a17a22bfd9825
Shorya Singh <shorya.4.singh@gmail.com>
2020-05-07 16:10:52 +0530

    spring security

--- menu-service/src/main/java/com/menu/MenuServiceApplication.java
+++ menu-service/src/main/java/com/menu/MenuServiceApplication.java
@@ -4,6 +4,7 @@ package com.menu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,7 +12,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableSwagger2
 public class MenuServiceApplication {
---
+++ menu-service/src/main/java/com/menu/security/SpringConfig.java
@@ -0,0 +1,43 @@
+package com.menu.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.netflix.ribbon.proxy.annotation.Http.HttpMethod;
+
+@Configuration
+@EnableWebSecurity
+public class SpringConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		auth.inMemoryAuthentication()
+        .withUser("user").password("{noop}password").roles("USER")
+        .and()
+        .withUser("admin").password("{noop}password").roles("ADMIN")
+		.and()
+		.withUser("manager").password("{noop}manager").roles("USER","MANAGER","ADMIN");
+	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		http
+        //HTTP Basic authentication
+        .httpBasic()
+        .and()
+        .authorizeRequests()
+       // .antMatchers(HttpMethod.GET, "/menu/**").hasRole("USER")
+       // .antMatchers(HttpMethod.GET,"/memu").hasRole("USER")
+     //   .antMatchers(HttpMethod.POST, "/menu").hasRole("ADMIN")
+        //.antMatchers(HttpMethod.PUT, "/menu/**").hasRole("ADMIN")
+     //   .antMatchers(HttpMethod.DELETE, "/menu/**").hasRole("ADMIN")
+        .and()
+        .csrf().disable()
+        .formLogin().disable();
+
+}
+}
